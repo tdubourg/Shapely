f7eb5f7fcf419460f5070ad41ed8cff49155b60e
tdubourg <plop@plop.fake>
2013-05-12 13:44:49 -0700

    Added a message with the dependencies for building custom-Shapely

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+echo "Please be sure you have the following packets installed: python-setuptools, cython, python-dev, libgeos-dev."
+echo  "Press ENTER if yes, CTRL + C if no (and install them!)"
+read
+
 python setup.py build sdist && sudo python setup.py install
 
